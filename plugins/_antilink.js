762c999b78a2a1d378087b22a675ff326c02a920
Bayuxz <107552948+Bayuxz@users.noreply.github.com>
2022-07-06 07:33:40 +0700

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -8,12 +8,12 @@ handler.before = async function (m, { user, isBotAdmin, isAdmin }) {
 
   if (chat.antiLink && isGroupLink) {
     await m.reply(`*「 ANTI LINK 」*\n\nDetected *${await conn.getName(m.sender)}* you have sent the group link!\n\nSorry you will be kicked from this group byee!`)
-    if (isAdmin) return m.reply('*wah anjir , situ admin gabisa dikik 😢. hehe..*')
-    if (!isBotAdmin) return m.reply('*Bot nya bukan admin, mana bisa ngekik 😐*')
+    if (isAdmin) return m.reply('*anjer, Lu admin jadi gabisa dikik🗿, wkwkwkwk!*')
+    if (!isBotAdmin) return m.reply('*Gw bukan admin banh, Gimana mau ngekik*')
     let linkGC = ('https://chat.whatsapp.com/' + await conn.groupInviteCode(m.chat))
     let isLinkconnGc = new RegExp(linkGC, 'i')
     let isgclink = isLinkconnGc.test(m.text)
-    if (isgclink) return m.reply('*「 ANTI LINK 」*\n\nItu Dilarang❗, Maaf kamu akan saya kik.\nKarena kirim Link👍')
+    if (isgclink) return m.reply('*「 ANTI LINK 」*\n\nItu Dilarang❗, Maaf kamuh akan saya kik.\nKarena kirim Link, Yahaha.')
     await conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
   }
   return true
